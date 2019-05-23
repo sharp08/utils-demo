d6f21e3175bb736dad1adb22f73f984797110595
kaka <1003345627@qq.com>
2019-05-23 19:49:10 +0800

    ...

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <div class="content-v left">
         <h3>方法列表</h3>
         <ul class="list-wrap">
-          <li v-for="(item,idx) in utilsList" :key="item" @click="click(item)">{{item}}</li>
+          <li :key="item" @click="click(item)" v-for="(item,idx) in utilsList">{{item}}</li>
         </ul>
       </div>
       <div class="content-v right">
@@ -23,8 +23,8 @@
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
-import Mock from "mockjs";
+import HelloWorld from "@/components/HelloWorld.vue"
+import Mock from "mockjs"
 
 export default {
   name: "home",
@@ -36,41 +36,41 @@ export default {
       utilsList: Object.keys(ktools), //  工具方法名列表
       desc: "", //  方法描述
       dispatchStr: "" //  调用区展示
-    };
+    }
   },
   beforeCreate() {
-    window.ktools = ktools;
+    window.ktools = ktools
   },
   mounted() {},
   methods: {
     click(name) {
       switch (name) {
         case "fmtDeepClone":
-          this.fmtDeepCloneDemo("fmtDeepClone");
-          break;
+          this.fmtDeepCloneDemo("fmtDeepClone")
+          break
         case "fmtDate":
-          this.fmtDateDemo("fmtDate");
-          break;
+          this.fmtDateDemo("fmtDate")
+          break
       }
     },
     //  时间格式化
     fmtDateDemo(name) {
-      let a = new Date();
-      console.group(name);
-      console.log(`原始数据：`, a);
-      this.desc = `ktools.fmtDate(Date|String)；时间格式化，默认 yyyy-MM-dd HH:mm:ss`;
+      let a = new Date()
+      console.group(name)
+      console.log(`原始数据：`, a)
+      this.desc = `ktools.fmtDate(Date|String)；时间格式化，默认 yyyy-MM-dd HH:mm:ss`
       this.dispatchStr = `ktools.fmtDate(new Date());
-ktools.fmtDate("2018-12-21");`;
-      let b = ktools.fmtDate(a);
-      console.log(b);
-      console.groupEnd(name);
+ktools.fmtDate("2018-12-21");`
+      let b = ktools.fmtDate(a)
+      console.log(`新数据：`, b)
+      console.groupEnd(name)
     },
     //  深拷贝
     fmtDeepCloneDemo(name) {
-      Mock.Random.cname();
-      Mock.Random.email();
-      Mock.Random.province();
-      Mock.Random.date();
+      Mock.Random.cname()
+      Mock.Random.email()
+      Mock.Random.province()
+      Mock.Random.date()
       let originData = Mock.mock([
         {
           name: "@cname",
@@ -94,21 +94,21 @@ ktools.fmtDate("2018-12-21");`;
             }
           ]
         }
-      ]);
-      console.group(name);
-      console.log(`原始数据：`, originData);
+      ])
+      console.group(name)
+      console.log(`原始数据：`, originData)
       let clone = ktools.fmtDeepClone(originData, {
         name: "new_name", //  原对象 name 映射到新对象的 new_name
         //  原对象 birth 映射到新对象 birthday，并且对值进行修改
         birth: (val, obj) => {
           return {
             birthday: `${obj.name}_${val}`
-          };
+          }
         },
         location: "position" //  原对象 location 映射到新对象的 position
-      });
+      })
       //  描述展示
-      this.desc = `ktools.fmtDeepClone(Object[,Object])；深拷贝并处理，支持修改 key 和 val。`;
+      this.desc = `ktools.fmtDeepClone(Object[,Object])；深拷贝并处理，支持修改 key 和 val。`
       //  调用区展示
       this.dispatchStr = `ktools.fmtDeepClone(originData, {
         name: "new_name", //  原对象 name 映射到新对象的 new_name
@@ -119,12 +119,12 @@ ktools.fmtDate("2018-12-21");`;
           };
         },
         location: "position" //  原对象 location 映射到新对象的 position
-  });`;
-      console.log(`新数据：`, clone);
-      console.groupEnd(name);
+  });`
+      console.log(`新数据：`, clone)
+      console.groupEnd(name)
     }
   }
-};
+}
 </script>
 <style lang="less" scoped>
 .borderStyle() {
